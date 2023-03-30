244f324226c006cb6ad8accfa213e682ac4d150b
beckdeyoung816 <beckdeyoung@gmail.com>
2023-03-29 20:13:05 -0400

    Creating board and pieces

--- Scripts/block_class.py
+++ Scripts/block_class.py
@@ -1,47 +1,90 @@
 import pygame
 ## Write a class for a tetris block
 class block():
-    def init(self, x, y, color):
-        self.x = x
-        self.y = y
+    def __init__(self, color):
+        # self.x = x
+        # self.y = y
         self.color = color
         
         if color == 'Purple':
             self.shape = [[1, 1, 1, 1]]
+            self.coords = [(0, 0), (1, 0), (2, 0), (3, 0)]
             self.rgb = (128, 0, 128)
         elif color == 'Dark Green':
-            self.shape = [[0, 0, 1, 1], [0, 1, 1, 1]]
+            self.shape = [[0, 0, 1, 1], 
+                          [1, 1, 1]]
+            self.coords = [(2,1), (3,1),
+                           (0, 0), (1, 0), (2, 0)]
             self.rgb = (0, 128, 0)
         elif color == 'Dark Blue':
-            self.shape = [[1, 1, 1, 1], [1, 0, 0, 0]]
+            self.shape = [[1, 0, 0, 0], 
+                          [1, 1, 1, 1]]
+            self.coords = [(0, 1), 
+                           (0, 0), (1, 0), (2, 0), (3, 0)]
             self.rgb = (0, 0, 128)
         elif color == "Orange":
-            self.shape = [[1, 1, 1], [1, 0, 0]]
+            self.shape = [[1, 0, 0], 
+                          [1, 1, 1]]
+            self.coords = [(0, 1), 
+                           (0, 0), (1, 0), (2, 0)]
             self.rgb = (255, 165, 0)
         elif color == "Red":
-            self.shape = [[1, 1, 1], [0, 1, 1]]
+            self.shape = [[0, 1, 1], 
+                          [1, 1, 1]]
+            self.coords = [(1, 1), (2, 1), 
+                           (0, 0), (1, 0), (2, 0)]
             self.rgb = (255, 0, 0)
         elif color == "Yellow":
-            self.shape = [[1, 0, 1], [1, 1, 1]]
+            self.shape = [[1, 0, 1], 
+                          [1, 1, 1]]
+            self.coords = [(0, 1), (2, 1), 
+                           (0, 0), (1, 0), (2, 0)]
             self.rgb = (255, 255, 0)
         elif color == "Light Green": 
-            self.shape = [[1, 1], [1, 1]]
+            self.shape = [[1, 1], 
+                          [1, 1]]
+            self.coords = [(0, 1), (1, 1), 
+                           (0, 0), (1, 0)]
             self.rgb = (0, 255, 0)
         elif color == "White":
-            self.shape = [[1, 1], [1, 0]]
+            self.shape = [[1, 1], 
+                          [1, 0]]
+            self.coords = [(0, 1), (1, 1),
+                            (0, 0)]
             self.rgb = (255, 255, 255)
         elif color == "Gray":
-            self.shape = [[0, 1, 0], [1, 1, 1], [0, 1, 0]]
+            self.shape = [[0, 1, 0], 
+                          [1, 1, 1], 
+                          [0, 1, 0]]
+            self.coords = [(1, 2), 
+                           (0, 1), (1, 1), (2, 1),
+                           (1, 0)]
             self.rgb = (128, 128, 128)
         elif color == "Light Pink":
-            self.shape  = [[1, 1, 1, 1], [0, 1, 0, 0]]
+            self.shape  = [[0, 1, 0, 0], 
+                           [1, 1, 1, 1]]
+            self.coords = [(0, 1), 
+                           (0, 0), (1, 0), (2, 0), (3, 0)]
             self.rgb = (255, 192, 203)
         elif color == "Light Blue":
-            self.shape = [[1, 1, 1], [1, 0, 0], [1, 0, 0]]
+            self.shape = [[1, 0, 0], 
+                          [1, 0, 0], 
+                          [1, 1, 1]]
+            self.coords = [(0, 2),
+                           (0, 1),
+                           (0, 0), (1, 0), (2, 0)]
             self.rgb = (0, 255, 255)
         elif color == "Dark Pink":
-            self.shape = [[1, 1, 0], [0, 1, 1], [0, 0, 1]]
+            self.shape = [[1, 0, 0], 
+                          [1, 1, 0], 
+                          [0, 1, 1]]
+            self.coords = [(0, 2),
+                           (0, 1), (1, 1),
+                           (1, 0), (2, 0)]
             self.rgb = (255, 20, 147)
+        
+        # Define Rectangles for shape
+        self.rects = [pygame.Rect(x, y, 1, 1) for x,y in self.coords]
         
     def rotate(self):
         self.shape = zip(*self.shape[::-1])
@@ -54,7 +97,3 @@ class block():
                     
                     
 
-# Create list of items where each item is the set of coordinates for a given kanoodle piece where the first index is the center of rotation for the piece
-pieces = [[(0,0), (0,-1), (0,-2), (0,1)], # Purple
-          [(0,0), (0,-1), (0,-2), (1,-2)], # Dark Green
-          ]
--- Scripts/kanoodle.py
+++ Scripts/kanoodle.py
@@ -6,37 +6,56 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GRAY = (128, 128, 128)
 
-# Set the size of the screen
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-
 # Set the size of the grid
-GRID_WIDTH = 10
-GRID_HEIGHT = 6
+W,H = 10, 6
 
 # Set the size of each block
-BLOCK_SIZE = 40
+BLOCK_SIZE = 45
 
 # Set the size of the margin between blocks
-MARGIN = 5
+MARGIN = 5 #5
+
+RES_W, RES_H = 800, 600
+FPS = 60
+
+# Set the margin between the board and the edge of the screen
+# The x margin should center the board
+X_MARGIN = (RES_W - (BLOCK_SIZE + MARGIN) * W - MARGIN) // 2
+Y_MARGIN = 20
 
 # Set the size of the board
-BOARD_WIDTH = (BLOCK_SIZE + MARGIN) * GRID_WIDTH + MARGIN
+BOARD_WIDTH = (BLOCK_SIZE + MARGIN) * W + MARGIN
+
+# Create list of items where each item is the set of coordinates for a given kanoodle piece where the first index is the center of rotation for the piece
+
+blocks = [[(0,0), (1,0), (2,0), (3,0)], # Purple
+          [(0,0), (0,-1), (0,-2), (1,-2)], # Dark Green
+          [(0,0), (1, 1), (1,-1), (-1,-1)] # Light Green
+          ]
+
+blocks = [block(color) for color in ["Purple", "Dark Green", "Light Green", "Red", "Yellow", "Dark Blue", "Orange", "Dark Pink", "Light Blue", "White", "Gray", "Light Pink"]]
+
+#figures = [[pygame.Rect(x + RES_W // 2 + X_MARGIN, y + 1 + Y_MARGIN, 1, 1) for x, y in fig_pos] for fig_pos in pieces]
+
+figure_rect = pygame.Rect(0, 0, BLOCK_SIZE, BLOCK_SIZE)
 
 # Create a grid
-grid = [pygame.Rect(MARGIN + (MARGIN + BLOCK_SIZE) * x, MARGIN + (MARGIN + BLOCK_SIZE) * y, BLOCK_SIZE, BLOCK_SIZE) for x in range(GRID_WIDTH) for y in range(GRID_HEIGHT)]
+grid = [pygame.Rect(X_MARGIN + (MARGIN + BLOCK_SIZE) * x, Y_MARGIN + (MARGIN + BLOCK_SIZE) * y, BLOCK_SIZE, BLOCK_SIZE) for x in range(W) for y in range(H)]
 
+
+block_1 = blocks[2]
 # Initialize Pygame
 pygame.init()
 
 # Set the size of the screen
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+sc = pygame.display.set_mode((RES_W, RES_H))
+# game_sc = pygame.Surface(RES_W, RES_H)
 clock = pygame.time.Clock()
 pygame.display.set_caption("Kanoodle")
 
 # Loop until the user clicks the close button
 while True:
-    screen.fill(pygame.Color('black'))
+    sc.fill(pygame.Color('black'))
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -44,7 +63,14 @@ while True:
             exit()
             
     # Draw the grid
-    [pygame.draw.rect(screen, GRAY, rect, 1) for rect in grid]
+    [pygame.draw.rect(sc, GRAY, rect, 1) for rect in grid]
+    
+    # Draw the figure
+    for i in range(len(block_1.rects)):
+        figure_rect.x = X_MARGIN + block_1.rects[i].x * (BLOCK_SIZE + MARGIN)
+        figure_rect.y = Y_MARGIN + block_1.rects[i].y * (BLOCK_SIZE + MARGIN) 
+        pygame.draw.rect(sc, block_1.rgb, figure_rect)
+
             
     pygame.display.flip()
     clock.tick()
