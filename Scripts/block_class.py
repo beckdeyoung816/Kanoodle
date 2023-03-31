850f7a893c2c814d7c17208816e5988e6ec18a00
beckdeyoung816 <beckdeyoung@gmail.com>
2023-03-30 22:06:21 -0400

    Working rotation and buttons

--- Scripts/block_class.py
+++ Scripts/block_class.py
@@ -2,7 +2,9 @@ import pygame
 from config import BLACK, WHITE, GRAY, W, H, BLOCK_SIZE, MARGIN, RES_W, RES_H, X_MARGIN, Y_MARGIN, BOARD_WIDTH
 from copy import deepcopy
 import numpy as np
-## Write a class for a tetris block
+
+
+## Write a class for a Kanoodle block
 class block():
     def __init__(self, color):
         self.color = color
@@ -68,22 +70,22 @@ class block():
                            (0, 1), (1, 1),
                            (1, 0), (2, 0)]
             self.center_coord = (1,0)
-            
             self.rgb = (255, 20, 147)
         
         # Define Rectangles for shape
         self.rects = [pygame.Rect(x, y, 1, 1) for x,y in self.coords]
         
-        center = self.coords.index(self.center_coord)
-        self.rects[0], self.rects[center] = self.rects[center], self.rects[0]
-        
-        self.set_orientation()
+        self.dx, self.dy = 0,0
+        self.orientation = 1
+        self.board_coords = self.coords
     
     def set_orientation(self):
         self.height = max([y for x,y in self.coords])
         self.width = max([x for x,y in self.coords])
         self.x_mid = self.width // 2
         self.y_mid = self.height // 2
+        
+    
     
     def check_borders(self, field):
         for i in range(len(self.rects)):
@@ -101,77 +103,68 @@ class block():
         rects_old = deepcopy(self.rects)
         if dx == 0 and dy == 0:
             pass
-        elif dx == 1:
-            self.shift_right()
-        elif dx == -1:
-            self.shift_left()
-        elif dy == 1:
-            self.shift_up()
-        elif dy == -1:
-            self.shift_down()
+        elif dx > 0:
+            self.shift_right(dx)
+        elif dx < 0:
+            self.shift_left(-dx)
+        elif dy > 0:
+            self.shift_up(dy)
+        elif dy < 0:
+            self.shift_down(-dy)
         
-        if not self.check_borders(field):
+        if self.check_borders(field):
+            self.dx += dx
+            self.dy -= dy
+        else:    
             self.rects = rects_old
-     
-    def shift_up(self):
-        for i in range(len(self.rects)):
-            self.rects[i].y -= 1 
+
+    def shift_up(self, amt=1):
+        for rect in self.rects:
+            rect.y -= amt
     
-    def shift_down(self):
-        for i in range(len(self.rects)):
-            self.rects[i].y += 1
+    def shift_down(self, amt=1):
+        for rect in self.rects:
+            rect.y += amt
             
-    def shift_right(self):
-        for i in range(len(self.rects)):
-            self.rects[i].x += 1
+    def shift_right(self, amt=1):
+        for rect in self.rects:
+            rect.x += amt
 
-    def shift_left(self):
-        for i in range(len(self.rects)):
-            self.rects[i].x -= 1
+    def shift_left(self, amt=1):
+        for rect in self.rects:
+            rect.x -= amt
     
-    def flip(self, direction, field):
+    def rotate(self, direction, field):
         rects_old = deepcopy(self.rects)
-        if direction == "X":
-            self.rotate_90(1)
-        elif direction == "Y":
-            self.rotate_90(-1)
-    
+        coords_old = deepcopy(self.coords)
+        print(f'Old coords: {self.coords}')
+        print(f"Old rects: {self.rects}")
+        print(f'dx: {self.dx}, dy: {self.dy}')
+        if direction == "CW":
+            self.rotate_clockwise()
+        elif direction == "CCW":
+            self.rotate_counterclockwise()
+
+        print(f"Pre-border coords: {self.coords}")
+        print(f"Pre-border rects: {self.rects}")
         if not self.check_borders(field):
             self.rects = rects_old
-    # Reflect about the x-axis of the shape itself not the grid
-    
-    def rotate_90(self, direction):
-        center_coord = self.coords[0]
-        
-        # Define the angle of rotation (in radians)
-        theta = np.pi / 4 * direction
+            self.coords = coords_old
+        print(f'New coords: {self.coords}')
+        print("")
 
-        # Define the center point of the shape
-        center = np.array(center_coord).reshape((1, 2))
+    def rotate_clockwise(self):
+        print("Rotating Clockwise")
+        self.coords = [(y, -x) for x,y in self.coords]
+        for rect, new_coord in zip(self.rects, self.coords):
+             rect.x, rect.y = new_coord[0] + self.dx, new_coord[1] + self.dy
 
-        # Define the rotation matrix
-        cos_theta = np.cos(theta)
-        sin_theta = np.sin(theta)
-        rotation_matrix = np.array([[cos_theta, -sin_theta], [sin_theta, cos_theta]])
+    def rotate_counterclockwise(self):
+        print("Rotating Counterclockwise")
+        self.coords = [(-y, x) for x,y in self.coords]
+        for rect, new_coord in zip(self.rects, self.coords):
+             rect.x, rect.y = new_coord[0] + self.dx, new_coord[1] + self.dy
 
-        # Rotate each point around the center of the shape
-        rotated_shape = np.dot(np.array(self.coords) - center, rotation_matrix) + center
-
-        # Round the coordinates to the nearest integer
-        rotated_shape = np.round(rotated_shape).astype(int)
-        
-        self.coords = rotated_shape.tolist()
-        
-        self.rects = [pygame.Rect(x, y, 1, 1) for x,y in self.coords]
-        
-        # for i in range(len(self.rects)):
-        #     #self.rects[i].x = self.width - self.rects[i].x - 1
-        #     x = self.rects[i].y - center.x
-        #     y = self.rects[i].x - center.y
-        #     self.rects[i].x = center.x - x
-        #     self.rects[i].y = center.y + y
-        
-        # self.set_orientation()
             
     # Reflect about the y-axis
     def flip_y(self):
@@ -179,13 +172,4 @@ class block():
             self.rects[i].y = self.height - self.rects[i].y - 1
 
         self.set_orientation()
-    
-        
-    def draw(self, screen):
-        for i in range(len(self.shape)):
-            for j in range(len(self.shape[i])):
-                if self.shape[i][j] == 1:
-                    pygame.draw.rect(screen, self.color, (self.x + j * 20, self.y + i * 20, 20, 20))
-                    
-                    
 
--- Scripts/kanoodle.py
+++ Scripts/kanoodle.py
@@ -1,6 +1,5 @@
 import pygame
 from block_class import block
-from copy import deepcopy
 from config import BLACK, WHITE, GRAY, COLORS, W, H, BLOCK_SIZE, MARGIN, RES_W, RES_H, X_MARGIN, Y_MARGIN, BOARD_WIDTH
 
 # Create blocks
@@ -14,7 +13,7 @@ grid = [pygame.Rect(X_MARGIN + (MARGIN + BLOCK_SIZE) * x, Y_MARGIN + (MARGIN + B
 # Create an empty field
 field = [[0 for i in range(W)] for j in range(H)]
 
-figure = blocks[3]
+figure = blocks[0]
 
 # Initialize Pygame
 pygame.init()
@@ -25,7 +24,26 @@ sc = pygame.display.set_mode((RES_W, RES_H))
 clock = pygame.time.Clock()
 pygame.display.set_caption("Kanoodle")
 
+# Create buttons
+# define constants
+BUTTON_WIDTH = 100
+BUTTON_HEIGHT = 40
+BUTTON_SPACING = 10
+FONT_SIZE = 10
+# create font object
+font = pygame.font.SysFont('Arial', FONT_SIZE)
 
+# create list to store buttons
+buttons = []
+
+# create each button and add it to the list
+
+for i, color in enumerate(COLORS):
+    button_x = (RES_W - BUTTON_WIDTH) - 40
+    button_y = i * (BUTTON_HEIGHT + BUTTON_SPACING) + BUTTON_SPACING
+    button_rect = pygame.Rect(button_x, button_y, BUTTON_WIDTH, BUTTON_HEIGHT)
+    button_text = font.render(color, True, (0, 0, 0))
+    buttons.append((button_rect, button_text, color))
 
 # Loop until the user clicks the close button
 while True:
@@ -38,11 +56,11 @@ while True:
             exit()
         if event.type == pygame.KEYDOWN:
             if pygame.key.get_mods() & pygame.KMOD_SHIFT:
-                if event.key in [pygame.K_LEFT, pygame.K_RIGHT]:
-                    figure.flip("X", field)
-                elif event.key in [pygame.K_UP, pygame.K_DOWN]:
-                    figure.flip("Y", field)
-            if event.key == pygame.K_LEFT:
+                if event.key in [pygame.K_LEFT]:
+                    figure.rotate("CW", field)
+                elif event.key in [pygame.K_RIGHT]:
+                    figure.rotate("CCW", field)
+            elif event.key == pygame.K_LEFT:
                 dx = -1
             elif event.key == pygame.K_RIGHT:
                 dx = 1
@@ -50,6 +68,18 @@ while True:
                 dy = 1
             elif event.key == pygame.K_DOWN:
                 dy = -1
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            # check if a button was clicked
+            for button_rect, button_text, color in buttons:
+                if button_rect.collidepoint(event.pos):
+                    figure = block(color)
+                    # do something when the button is clicked
+    
+    # draw buttons
+    for button_rect, button_text, color in buttons:
+        pygame.draw.rect(sc, pygame.Color(WHITE), button_rect)
+        sc.blit(button_text, (button_rect.centerx - button_text.get_width() // 2, button_rect.centery - button_text.get_height() // 2))
+
     # Draw the grid
     [pygame.draw.rect(sc, GRAY, rect, 1) for rect in grid]
     
